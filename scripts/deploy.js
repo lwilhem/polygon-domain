dfa6e8b8a6f71e2ccae98365fe2f804cb03b8435
lwilhem <59793152+lwilhem@users.noreply.github.com>
2022-05-08 21:40:57 +0200

    :hammer: NFT Generation

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+const main = async () => {
+    // The first return is the deployer, the second is a random account
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+    const domainContract = await domainContractFactory.deploy();
+    await domainContract.deployed();
+    console.log("Contract deployed to:", domainContract.address);
+    console.log("Contract deployed by:", owner.address);
+  }
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
