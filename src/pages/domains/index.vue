cab6e6d4b722151bedb00dd46620b4bdae904be6
lwilhem <59793152+lwilhem@users.noreply.github.com>
2022-05-09 11:25:16 +0200

    :sparkles: domains list

--- src/pages/domains/index.vue
+++ src/pages/domains/index.vue
@@ -37,7 +37,7 @@ const { domain, record } = storeToRefs(store)
     </div>
   </section>
   <section class="flex flex-wrap toems-center justify-center">
-    <a v-for="data in store.mints" :key="data.id">
+    <a v-for="data in store.mints" :key="data.id" class="bg-indigo-500 text-slate-200 dark:bg-red-500 dark:text-zinc-900 px-8 py-3 mx-6 rounded-3">
       <span>{{ data.owner.slice(0,6) }}...{{ data.owner.slice(-4) }}</span>
     </a>
   </section>
