4b43da5f4728d89cfb56e313ad0005a75309f4b0
lwilhem <59793152+lwilhem@users.noreply.github.com>
2022-05-09 08:06:46 +0200

    :construction: UI

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { toggleDark } from '~/composables'
+
+const { t, availableLocales, locale } = useI18n()
+
+const toggleLocales = () => {
+  // change to some real logic
+  const locales = availableLocales
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
+</script>
+
+<template>
+  <header class="w-full h-16 flex items-center justify-evenly">
+    <RouterLink to="/" class="flex items-center justify-center">
+      <div class="i-mdi:glass-cocktail w-10 h-10" />
+      <h1 class="font-mono text-2xl">
+        On The Mints !
+      </h1>
+    </RouterLink>
+    <nav text-xl class="flex items-center justify-center">
+      <button class="icon-btn mx-2 !outline-none" :title="t('button.toggle_dark')" @click="toggleDark()">
+        <div i="carbon-sun dark:carbon-moon" />
+      </button>
+
+      <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
+        <div i-carbon-language />
+      </a>
+
+      <RouterLink class="icon-btn mx-2" to="/about" :title="t('button.about')">
+        <div i-carbon-dicom-overlay />
+      </RouterLink>
+
+      <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+        <div i-carbon-logo-github /></a>
+    </nav>
+  </header>
+</template>
--- src/pages/domains/index.vue
+++ src/pages/domains/index.vue
@@ -6,8 +6,8 @@ const { domain, record } = storeToRefs(store)
 </script>
 
 <template>
-  <section class="w-full h-fit  grid grid-cols-2 grid-rows-1 rounded-2 gap-5">
-    <object data="assets/form-input.svg" class="col-start-1 bg-indigo-500 col-end-2 w-full p-10 rounded-4" />
+  <section class="w-full my-5 h-fit grid grid-cols-2 grid-rows-1 rounded-2 gap-5">
+    <object data="assets/form-input.svg" class="col-start-1 bg-indigo-500 mx-4 col-end-2 w-full p-10 rounded-4" />
     <div class="col-start-2 col-end-3 bg-zinc-900 mx-4 flex flex-col p-10 rounded-4">
       <input v-model="domain" type="text" placeholder="Domain">
       <input v-model="record" type="text" placeholder="Records">
